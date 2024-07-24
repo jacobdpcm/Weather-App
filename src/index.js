2297901c781113bdde761f0fc889b69f8df7d066
JacobPCM <jacobd@postcardmania.com>
2024-07-24 17:49:06 -0400

    Figure out api data retrieval

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const weather = (function(){
+
+let currentParse = '';
+
+const getCurrentParse = async function(location, startDate, endDate){
+    const raw = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/clearwater?key=LK4KV98AZ6RNMYP4RLDSSSUWQ', {mode: 'cors'})
+    currentParse = await raw.json();
+}
+
+const showCurrentParce = function(){
+    console.log(currentParse);
+}
+
+return {getCurrentParse, showCurrentParce}
+})();
+
+weather.getCurrentParse()
+.then(weather.showCurrentParce());//Need to figure out the .then stuff to show it after it retrieves the data.
+
+
+
+
+
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY 
