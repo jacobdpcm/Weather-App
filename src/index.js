c9c0c73e22cf5e0d5ef576c4eb94efd5efec6586
JacobPCM <jacobd@postcardmania.com>
2024-07-26 16:57:10 -0400

    Establish realtime fetching of weather API data

--- src/index.js
+++ src/index.js
@@ -3,21 +3,48 @@ import './style.css';
 const weather = (function(){
 
 let currentParse = '';
+let address = '';
+let currentCondition = '';
+let currentTemp = '';
 
-const getCurrentParse = async function(location, startDate, endDate){
-    const raw = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/clearwater?key=LK4KV98AZ6RNMYP4RLDSSSUWQ', {mode: 'cors'})
+
+
+const generateCurrentParse = async function(location, startDate, endDate){
+    const raw = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?key=LK4KV98AZ6RNMYP4RLDSSSUWQ', {mode: 'cors'})
     currentParse = await raw.json();
+
+    //allocate data:
+    address = currentParse.resolvedAddress;
+    currentCondition = currentParse.currentConditions.conditions;
+    currentTemp = currentParse.currentConditions.temp + '°F';
 }
 
 const showCurrentParce = function(){
     console.log(currentParse);
 }
 
-return {getCurrentParse, showCurrentParce}
-})();
+const getaddress = () => {return address}
+const getcurrentCondition = () => {return currentCondition}
+const getcurrentTemp = () => {return currentTemp}
 
-weather.getCurrentParse()
-.then(weather.showCurrentParce());//Need to figure out the .then stuff to show it after it retrieves the data.
+return {generateCurrentParse, showCurrentParce, getaddress, getcurrentCondition, getcurrentTemp}
+})();
+const button = document.querySelector('.getWeather');
+const searchLocation = document.querySelector('#searchLocation')
+const address = document.querySelector('.address');
+const currentConditions = document.querySelector('.currentConditions');
+const temp = document.querySelector('.temp');
+
+button.addEventListener('click', () => {
+    weather.generateCurrentParse(searchLocation.value)
+    .then(() => {
+        address.textContent = weather.getaddress();
+        currentConditions.textContent = weather.getcurrentCondition();
+        temp.textContent = weather.getcurrentTemp();
+});
+})
+
+
 
 
 
